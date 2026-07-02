5c4c27991a486a213214f94fc74fa6714fc6ce87
InternalProjectsKARYA <internalprojectsdevelopment@gmail.com>
2026-07-02 15:43:19 +0530

    Add initial blog application structure and templates
    - Created the blogs application with necessary files including models, views, and templates.
    - Added admin configuration in admin.py.
    - Defined the BlogsConfig class in apps.py.
    - Implemented basic views for home, about, and contact pages.
    - Created HTML templates for home, about, and contact pages.
    - Added CSS for styling the blog layout.
    - Set up URL routing for the blog application.
    - Initialized database with SQLite.
    - Added necessary settings for the Django project.

---
+++ django_project/blogs/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+posts=[
+    {
+    'author': 'Sai Krishna',
+    'title': 'Blog Post 1', 
+    'content': 'This is the content of the first blog post.',
+    'date': 'October 1, 2023'
+},
+{
+    'author': 'Sai Krishna',
+    'title': 'Blog Post 1', 
+    'content': 'This is the content of the first blog post.',
+    'date': 'October 1, 2023'
+}
+]
+
+def home(request):
+    context={
+        'posts':posts,
+        'title':'Home Page'
+    }
+    return render(request,'blogs/home.html',context)
+
+def about(request):
+    return render(request,'blogs/about.html')
+def contact(request):
+    return render(request,'blogs/contact.html')
